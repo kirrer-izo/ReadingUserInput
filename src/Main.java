568aa4f619e45ac4cf9f3ace21a126711f7f6309
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-06 15:41:43 +0300

    modified the getInputFromScanner it uses Scanner class to get input from user and the calculate the age of the user

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         int currentYear = 2025;
 
+        try {
         System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
         System.out.println(getInputFromScanner(currentYear));
+        }
     }
 
     public static String getInputFromConsole(int currentYear) {
@@ -17,6 +22,19 @@ public class Main {
         return "So you are " + age + " years old";
     }
     public static String getInputFromScanner(int currentYear) {
-        return  "";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi, What's your Name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi, " + name + ", Thanks for taking the course!");
+
+        System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
+
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "So you are " + age + " years old";
+
     }
 }
