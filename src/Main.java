d782cb91a52d4c827615e6303ce6ecbd290203be
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-07 12:06:56 +0300

    created method named checkData of type int and accepts two parameters. The method checks if user has entered a valid year of birth and added error handling in the method named getInputFromScanner

--- src/Main.java
+++ src/Main.java
@@ -31,10 +31,35 @@ public class Main {
         System.out.println("Hi, " + name + ", Thanks for taking the course!");
 
         System.out.println("What year were you born? ");
-        String dateOfBirth = scanner.nextLine();
 
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+        boolean validDOb = false;
+        int age = 0;
+
+        do {
+            System.out.println("Enter a year of birth between " + (currentYear - 125) + " and " + (currentYear));
+
+            try {
+            age = checkData(currentYear, scanner.nextLine());
+            validDOb =  age < 0 ? false : true;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Characters not allowed!!! Try again.");
+            }
+        } while (!validDOb); {
         return "So you are " + age + " years old";
+        }
 
+
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if ((dob < minimumYear) || dob > currentYear) {
+            return -1;
+        }
+
+        return (currentYear - dob);
     }
 }
