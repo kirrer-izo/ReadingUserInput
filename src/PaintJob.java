3d47c8fa13266df6b76624171a9f2daf6858223f
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-07 22:20:05 +0300

    created a class named PaintJob, with method named getBucketCount with 4 parameters; all of type double width, height, areaPerBucket, extraBuckets. The method return a value of type int that represents the number of buckets of paint required before going to work

---
+++ src/PaintJob.java
@@ -0,0 +1,20 @@
+public class PaintJob {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int getBucketCount (double width, double height, double areaPerBucket ,int extraBuckets) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        double area = width * height;
+        double bucketsRequired = (area / areaPerBucket) - extraBuckets;
+
+
+        return (int) Math.ceil(bucketsRequired);
+    }
+
+}
