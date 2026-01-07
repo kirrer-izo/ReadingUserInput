c766748a8a7e8984032316228901d47ccb254bed
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-07 22:23:55 +0300

    created another overloaded method getBucketCount with 2 parameters namely area and areaPerBucket. The method returns value of type int that represents number of buckets required

--- src/PaintJob.java
+++ src/PaintJob.java
@@ -29,6 +29,13 @@ public class PaintJob {
         return (int) Math.ceil(bucketsRequired);
     }
 
+    public static int getBucketCount (double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        double bucketsRequired = area / areaPerBucket;
+        return (int) Math.ceil(bucketsRequired);
 
+    }
 
 }
